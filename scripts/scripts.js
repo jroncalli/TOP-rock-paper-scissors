96066a334e32ede11a2526c7f1c72bdd7ff1465b
jroncalli <jerryroncalli@gmail.com>
2022-07-18 20:30:30 -0700

    Partially completed

---
+++ scripts/scripts.js
@@ -0,0 +1,43 @@
+// Select elements
+const playerChoice = document.getElementById("pchoice");
+const compResult = document.getElementById("cresult");
+const choices = document.querySelectorAll("img");
+const pScoreDisplay = document.getElementById("scorep");
+const cScoreDisplay = document.getElementById("scorec");
+const winnerDisplay = document.getElementById("winner");
+
+const possibleChoice = ["Rock", "Paper", "Scissors"];
+
+let userChoice;
+let result;
+let compChoice;
+let pScore = 0;
+let cScore = 0;
+
+//Generate Computer Choice
+
+const computerChoice = function () {
+  compChoice = possibleChoice[Math.floor(Math.random() * possibleChoice.length)];
+  compResult.textContent = compChoice;
+};
+
+choices.forEach((choices) => {
+  choices.addEventListener("click", function (e) {
+    console.log(e.target.id);
+    userChoice = e.target.id;
+    playerChoice.textContent = userChoice;
+    computerChoice();
+    if (userChoice == compChoice) {
+      winnerDisplay.textContent = "Tie Game";
+    } else if (userChoice == "Rock")
+      if (compChoice == "Paper") {
+        winnerDisplay.textContent = "Computer Won";
+        cScore++;
+        cScoreDisplay.textContent = cScore;
+        checkWinner();
+      }
+  });
+});
+
+// Check for winner
+function checkWinner() {}
